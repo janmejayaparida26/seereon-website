be740e688f064c8b29ab4d7663bebc0aa5aeebf0
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-03-18 15:34:47 +0530

    I have changed some content in DesignFuture.jsx file

--- src/components/DesignFuture.jsx
+++ src/components/DesignFuture.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { ScrollFillText } from "./ScrollFillText";
+
+
 
 const DesignFuture = () => {
   return (
-    <section className="w-full bg-[#f0f0f0] !py-48 px-6 flex justify-center">
-      <div className="max-w-5xl text-center">
+    <section className="w-full bg-[#f0f0f0] !py-30 px-6 flex justify-center">
+      <div className="max-w-6xl text-center">
 
         {/* Heading */}
-        <h1 className="text-[110px] leading-[1.05] font-['Arimo',sans-serif] font-semibold text-black mb-10">
-          We design the
-          <br />
-          future with you!
+        <h1 className="text-[90px] leading-[1.05] font-['Arimo',sans-serif] font-semibold text-black mb-05">
+        <ScrollFillText>Technology Built Around<br /> Your Business</ScrollFillText>
         </h1>
 
         {/* Subheading */}
@@ -18,12 +19,10 @@ const DesignFuture = () => {
         </p>
 
         {/* Description */}
-        <p className="text-[18px] font-['Arimo',sans-serif] leading-[1.7] text-black/80">
-          SEEREON is a forward-thinking technology partner that empowers businesses 
-          to envision and build their digital future. We help organizations unlock excellence, 
-          articulate their digital presence, and step into the next era with scalable software 
-          and strategic IT solutions. Our work goes beyond simple coding—it fosters innovation-led 
-          momentum, equips brands with future-ready tools, and defines pathways for sustainable digital success.
+        <p className="text-[18px] font-['Arimo',sans-serif] leading-[1.7] px-10  text-black/80">
+          SEEREON is a technology partner focused on delivering scalable, high-performance software systems for enterprises.
+          We go beyond development by aligning every solution with your business goals—ensuring that the systems we build are not only functional, but strategically impactful and future-ready.
+          Our approach combines structured engineering, modern technologies, and deep understanding of business workflows.
         </p>
 
       </div>
