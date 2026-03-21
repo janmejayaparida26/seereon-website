022a1afe59d78345342aef66186904ee66873567
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-03-21 12:48:29 +0530

    I have changed some content in ContactBanner.jsx file

--- src/pages/contact/ContactBanner.jsx
+++ src/pages/contact/ContactBanner.jsx
@@ -14,11 +14,11 @@ export default function CultureSection() {
   const textY = useTransform(scrollYProgress, [0, 1], ["0%", "40%"]);
 
   return (
-    <section className="px-6 py-16 mt-10 bg-[#f0f0f0]">
+    <section className="px-0 py-0 mt-0 bg-[#f0f0f0]">
 
       <div
         ref={ref}
-        className="relative w-full h-[420px] overflow-hidden rounded-[40px]"
+        className="relative w-full h-[420px] overflow-hidden"
       >
 
         {/* Background Image */}
@@ -26,7 +26,7 @@ export default function CultureSection() {
           style={{ y: imageY }}
           src="https://images.unsplash.com/photo-1423666639041-f56000c27a9a?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Contact"
-          className="absolute inset-0 w-full h-[120%] object-cover"
+          className="absolute top-[-15%] inset-0 w-full h-[120%] object-cover"
         />
 
         {/* Dark Overlay */}
@@ -34,7 +34,7 @@ export default function CultureSection() {
 
         {/* Center Text */}
         <motion.h1
-          style={{ }}
+          style={{}}
           className="absolute inset-0 flex items-center justify-center text-white text-[90px] font-medium"
         >
           Contact
