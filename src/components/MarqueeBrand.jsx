f9396e4512d3aaf987686a7cdda8a687b2743db0
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-03-23 15:25:14 +0530

    I have changed some content in MarqueeBrand.jsx file

--- src/components/MarqueeBrand.jsx
+++ src/components/MarqueeBrand.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const Marquee = () => {
-  const text = "We are Seereon"; 
+  const text = "What Our Clients Have to Say!"; 
 
   return (
     <div className="w-full overflow-hidden bg-[#222222] pt-24 pb-10 md:pt-60 md:pb-10 text-white">
@@ -16,13 +16,13 @@ const Marquee = () => {
         initial={{ x: 0 }}
         animate={{ x: "-50%" }} 
         transition={{ 
-          duration: 5, 
+          duration: 10, 
           ease: "linear", 
           repeat: Infinity 
         }}
       >
         {[...Array(9)].map((_, i) => (
-          <span key={i} className="flex gap-x-4 md:gap-x-8">
+          <span key={i} className="flex gap-x-14 md:gap-x-8">
             {text.split(" ").map((word, index) => (
               <span key={index}>{word}</span>
             ))}
