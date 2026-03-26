47bc84f03a0cfc5a840781c8be4e88c4a696a182
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-03-26 10:19:32 +0530

    I have changed some content in ServiceBanner.jsx file

--- src/pages/services/ServiceBanner.jsx
+++ src/pages/services/ServiceBanner.jsx
@@ -44,7 +44,7 @@ const ServiceBanner = () => {
           className="relative z-10 flex flex-col items-center px-4"
           style={{ opacity }}
         >
-          <h1 className="text-white text-5xl sm:text-7xl lg:text-8xl xl:text-7xl font-['Arimo',sans-serif] font-bold text-center tracking-tighter select-none">
+          <h1 className="text-white text-5xl sm:text-7xl lg:text-8xl xl:text-8xl font-['Arimo',sans-serif] font-bold text-center tracking-tighter select-none">
             Services
           </h1>
           
