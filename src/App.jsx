66ef1e702746040dc8f4f0b60e51d71dcaddc851
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-03-30 10:34:11 +0530

    I have create a component CustomSoftware.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Career from "./pages/career/Career";
 import AboutUs from "./pages/aboutus/AboutUs";
 import TeamPage from "./pages/teams/TeamPage";
 import Services from "./pages/services/ServicesPage";
-import ServiceInner from "./pages/services/ServiceInner";
+// import ServiceInner from "./pages/services/ServiceInner";
 import Work from "./pages/work/Work";
 import Product from "./pages/product/Product";
 function App() {
