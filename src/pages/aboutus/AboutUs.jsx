cf978c71f376bfbb1ba77e792fc18c11a990cbfd
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-04-28 11:03:23 +0530

    I have changed some code in AboutUs.jsx file

--- src/pages/aboutus/AboutUs.jsx
+++ src/pages/aboutus/AboutUs.jsx
@@ -15,12 +15,6 @@ import ContactCTA from "../../components/ContactCTA";
 const AboutUs = () => {
   return (
     <>
-    <Helmet>  {/* ← Helmet goes HERE, inside return */}
-        <title>About Us – Seereon</title>
-        <meta name="description" content="Learn about Seereon's team and mission." />
-        <link rel="canonical" href="https://seereon.co.in/aboutus" />
-      </Helmet>
-
       <CultureAbout />
       <FirstSectionabout />
       <SecondSectionabout />
