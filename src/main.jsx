d7380dccdf7b089210b2301e333b725e799b0cc7
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-04-28 10:37:12 +0530

    I have changed and add some code in package-lock.json, package.json and main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
 
 import "./index.css";
 import App from "./App.jsx";
